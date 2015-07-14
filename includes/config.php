32033d613e3b3077e331b09cbbf7e7d7ae75b615
juancho637 <Juancho28@MacBook-Pro-de-juan.local>
2015-07-14 12:10:05 -0500

    actualizacion
    actualizacion 1

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+    /*define('SQL_HOST','localhost');
+    define('SQL_USER','root');
+    define('SQL_PASS','root');
+    define('SQL_DB','bd_expofruits');*/
+
+    define('SQL_HOST','http://185.28.21.157');
+    define('SQL_USER','u843607827_elect');
+    define('SQL_PASS','Electricos123');
+    define('SQL_DB','u843607827_elect');
+?>
